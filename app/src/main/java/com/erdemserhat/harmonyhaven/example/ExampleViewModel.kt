bb4c583b0b186baf6027c5d125630206ba8f1868
Serhat <me.serhaterdem@gmail.com>
2024-03-09 20:36:39 +0300

    --server endpoint bugs solved. --login/register features (server-client) integration

--- app/src/main/java/com/erdemserhat/harmonyhaven/example/ExampleViewModel.kt
+++ app/src/main/java/com/erdemserhat/harmonyhaven/example/ExampleViewModel.kt
@@ -3,6 +3,7 @@ package com.erdemserhat.harmonyhaven.example
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.erdemserhat.harmonyhaven.domain.model.User
 import com.erdemserhat.harmonyhaven.domain.model.UserLogin
 import com.erdemserhat.harmonyhaven.network.UserApiService
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,8 +21,8 @@ class ExampleViewModel @Inject constructor(
     }
 
 
-    fun postUser() {
-        val mockUserModel = UserLogin("ali@ex2ample.com", "ali123")
+    fun loginUser() {
+        val mockUserModel = UserLogin("me.serhaterdem@gmail.com", "123A.sadasd3fsdA")
         viewModelScope.launch {
 
             try {
@@ -31,6 +32,42 @@ class ExampleViewModel @Inject constructor(
                     Log.d("erdem3451", a.body()!!.email)
                 } else {
                     Log.d("erdem3451", a.message())
+                }
+
+
+            } catch (e: Exception) {
+                Log.d("erdem3451", e.message.toString())
+            }
+
+
+        }
+
+
+
+    }
+
+    fun registerUser() {
+        val mockRegisterUser = User(
+            id = 0,
+            name = "Jack",
+            surname = "Gabirella",
+            password = "ADASDcc.3451.",
+            email = "gabriellad23@gmal.com",
+            gender = "Male",
+            profilePhotoPath = "-"
+
+
+        )
+        viewModelScope.launch {
+
+            try {
+                val a = userApiService.register(mockRegisterUser)
+                //Log.d("erdem3451", a.body()?.message.toString())
+                if (a.isSuccessful) {
+                    Log.d("erdem3451", a.body()?.message.toString())
+                } else {
+
+                    Log.d("erdem3451", "Erdem2")
                 }
 
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
     // Top-level build file where you can add configuration options common to all sub-projects/modules.
     plugins {
+        application
         id("com.android.application") version "8.2.2" apply false
         id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+        kotlin("jvm") version "1.4.21"
+        kotlin("plugin.serialization") version "1.4.21"
 
         //KSP
         //id("com.google.devtools.ksp") version "1.9.0-1.0.11" apply false
