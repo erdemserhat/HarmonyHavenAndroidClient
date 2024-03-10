5f3478ca609ba4925855779a7f1462f06a1ec47c
Serhat <me.serhaterdem@gmail.com>
2024-03-10 17:22:40 +0300

    --user use case tests 1.0

--- app/src/main/java/com/erdemserhat/harmonyhaven/domain/usecase/users/RegisterUser.kt
+++ app/src/main/java/com/erdemserhat/harmonyhaven/domain/usecase/users/RegisterUser.kt
@@ -1,13 +1,19 @@
 package com.erdemserhat.harmonyhaven.domain.usecase.users
 
 import com.erdemserhat.harmonyhaven.data.network.UserApiService
+import com.erdemserhat.harmonyhaven.domain.model.RequestResult
 import com.erdemserhat.harmonyhaven.domain.model.User
 import javax.inject.Inject
 
 class RegisterUser @Inject constructor(
     private val userApiService: UserApiService
 ) {
-    suspend operator fun invoke(newUser:User){
-        userApiService.register(newUser)
+    suspend operator fun invoke(newUser:User):RequestResult{
+        val response = userApiService.register(newUser)
+
+        val result = response.body()?.result ?:false
+        val message = response.body()?.message ?:"En error occurred"
+
+        return RequestResult(result,message)
     }
 }
